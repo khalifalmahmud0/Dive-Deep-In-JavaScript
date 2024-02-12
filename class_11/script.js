c03580e87ae7cb795b0e8522bd28a0d0b3c1079a
Khalif AL Mahmud <khalif@Khalifs-MacBook-Pro.local>
2024-02-12 21:44:30 +0600

    12 Feb 2024

---
+++ class_11/script.js
@@ -0,0 +1,48 @@
+// Conditional
+// Jodi = if
+// Tahle = else
+
+/*
+Rakib Bhai Jodi 30 bocor hoy tahole bia korbe , na hole bekar thakbe;
+*/
+
+// let rakibAge = 40;
+// if (rakibAge >= 30) {
+//   console.log("Rakib Bia Korbe");
+// }
+// else if () {}
+// else {
+//   console.log("Bekar thakbe");
+// }
+
+/*
+# rakib age , 18 kom hole , -> studnent
+# jodi 18 - 25 ,,, -> job kore 
+# 26 - 35 > bia korcha 
+# konotai holo na > expired
+*/
+
+// let rakibAge = 90;
+
+// if (rakibAge < 18) {
+//   console.log("He is a Student");
+// } else if (rakibAge >= 18 && rakibAge <= 25) {
+//   console.log("job kore");
+// } else if (rakibAge >= 26 && rakibAge <= 35) {
+//   console.log("Bia korbe");
+// } else {
+//   console.log("Expired");
+// }
+
+// 0 = falsey
+// 1 = Truthy
+
+if (true) {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+true ? console.log("True") : console.log("False");
+
+// true ? console.log("True") : console.log("False");
